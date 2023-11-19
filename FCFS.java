c929b7e68ddb5a0978cc10d8c12616e2cde41d68
Palash Dhabale <100768184+PalashDhabale@users.noreply.github.com>
2023-11-20 00:20:36 +0530

    Add files via upload

--- FCFS.java
+++ FCFS.java
@@ -1,68 +1,87 @@
-//FCFS Scheduling Algorithm:
-import java.util.*;
+// Java implementation of FIFO page replacement 
+// in Operating Systems. 
 
-public class FCFS {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter no of process: ");
-        int n = sc.nextInt();
-        int pid[] = new int[n]; // process ids
-        int ar[] = new int[n]; // arrival times
-        int bt[] = new int[n]; // burst or execution times
-        int ct[] = new int[n]; // completion times
-        int ta[] = new int[n]; // turn around times
-        int wt[] = new int[n]; // waiting times
-        int temp;
-        float avgwt = 0, avgta = 0;
+import java.util.HashSet; 
+import java.util.LinkedList; 
+import java.util.Queue; 
 
-        for (int i = 0; i < n; i++) {
-            System.out.println("enter process " + (i + 1) + " arrival time: ");
-            ar[i] = sc.nextInt();
-            System.out.println("enter process " + (i + 1) + " brust time: ");
-            bt[i] = sc.nextInt();
-            pid[i] = i + 1;
-        }
 
-        // sorting according to arrival times
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - (i + 1); j++) {
-                if (ar[j] > ar[j + 1]) {
-                    temp = ar[j];
-                    ar[j] = ar[j + 1];
-                    ar[j + 1] = temp;
-                    temp = bt[j];
-                    bt[j] = bt[j + 1];
-                    bt[j + 1] = temp;
-                    temp = pid[j];
-                    pid[j] = pid[j + 1];
-                    pid[j + 1] = temp;
-                }
-            }
-        }
-        // finding completion times
-        for (int i = 0; i < n; i++)
+public class FCFS
+{ 
+	// Method to find page faults using FIFO 
+	static int pageFaults(int pages[], int n, int capacity) 
+	{ 
+		// To represent set of current pages. We use 
+		// an unordered_set so that we quickly check 
+		// if a page is present in set or not 
+		HashSet<Integer> s = new HashSet<>(capacity); 
+	
+		// To store the pages in FIFO manner 
+		Queue<Integer> indexes = new LinkedList<>() ; 
+	
+		// Start from initial page 
+		int page_faults = 0; 
+		for (int i=0; i<n; i++) 
+		{ 
+			// Check if the set can hold more pages 
+			if (s.size() < capacity) 
+			{ 
+				// Insert it into set if not present 
+				// already which represents page fault 
+				if (!s.contains(pages[i])) 
+				{ 
+					s.add(pages[i]); 
+	
+					// increment page fault 
+					page_faults++; 
+	
+					// Push the current page into the queue 
+					indexes.add(pages[i]); 
+				} 
+			} 
+	
+			// If the set is full then need to perform FIFO 
+			// i.e. remove the first page of the queue from 
+			// set and queue both and insert the current page 
+			else
+			{ 
+				// Check if current page is not already 
+				// present in the set 
+				if (!s.contains(pages[i])) 
+				{ 
+					//Pop the first page from the queue 
+					int val = indexes.peek(); 
+	
+					indexes.poll(); 
+	
+					// Remove the indexes page 
+					s.remove(val); 
+	
+					// insert the current page 
+					s.add(pages[i]); 
+	
+					// push the current page into 
+					// the queue 
+					indexes.add(pages[i]); 
+	
+					// Increment page faults 
+					page_faults++; 
+				} 
+			} 
+		} 
+	
+		return page_faults; 
+	} 
+	
+	// Driver method 
+	public static void main(String args[]) 
+	{ 
+		int pages[] = {7, 0, 1, 2, 0, 3, 0, 4, 
+						2, 3, 0, 3, 2}; 
 
-        {
-            if (i == 0) {
-                ct[i] = ar[i] + bt[i];
-            } else {
-                if (ar[i] > ct[i - 1]) {
-                    ct[i] = ar[i] + bt[i];
-                } else
-                    ct[i] = ct[i - 1] + bt[i];
-            }
-            ta[i] = ct[i] - ar[i]; // turnaround time= completion time- arrival time
-            wt[i] = ta[i] - bt[i]; // waiting time= turnaround time- burst time
-            avgwt += wt[i]; // total waiting time
-            avgta += ta[i]; // total turnaround time
-        }
-        System.out.println("\npid arrival brust complete turn waiting");
-        for (int i = 0; i < n; i++) {
-            System.out.println(pid[i] + " \t " + ar[i] + "\t" + bt[i] + "\t" + ct[i] +
-                    "\t" + ta[i] + "\t" + wt[i]);
-        }
-        sc.close();
-        System.out.println("\naverage waiting time: " + (avgwt / n)); // printing average waiting time.
-        System.out.println("average turnaround time:" + (avgta / n)); // printing average turnaround time.
-    }
-}
+		int capacity = 4; 
+		System.out.println(pageFaults(pages, pages.length, capacity)); 
+	} 
+} 
+// This code is contributed by Gaurav Miglani 
+
